88b610fef307ef4719b2707b87e2e7a86520ce6d
Tola-D <gadetola2004@yahoo.co.uk>
2024-08-20 11:22:56 +0100

    updated version

--- Sorting.py
+++ Sorting.py
@@ -1,5 +1,5 @@
 # Sorting a list
-mylist = [23, 5, 30, -8, 10, 15, 50, 31]
+mylist = [23, 5, 30, -8, 10, 15, 50, 31, -22, 22]
 length = len(mylist)
 for i in range(length):
     for j in range(length-i-1):
