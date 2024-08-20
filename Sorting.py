3eebce50f3075b004ceed6ef316ccaaa329b3093
Tola-D <gadetola2004@yahoo.co.uk>
2024-08-20 10:35:10 +0100

    first commit from git cli

---
+++ Sorting.py
@@ -0,0 +1,8 @@
+# Sorting a list
+mylist = [23, 5, 30, -8, 10, 15, 50, 31]
+length = len(mylist)
+for i in range(length):
+    for j in range(length-i-1):
+        if mylist[j] > mylist[j+1]:
+            mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+print(mylist)            
